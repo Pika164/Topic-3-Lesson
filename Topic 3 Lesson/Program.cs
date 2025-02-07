918258bf7814286b33cc687d289c21736f608b09
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-07 14:38:53 -0500

    Add project files.

---
+++ Topic 3 Lesson/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Topic_3_Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        String name, eyeColor, HairColor;
+            int age, height, weight;
+        }
+    }
+}
