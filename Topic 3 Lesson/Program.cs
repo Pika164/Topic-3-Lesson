d8019ada2b4b277fd0dc61324a6669d452148645
Linkin Krysa <339742660@GoTVDSB.ca>
2025-02-10 13:53:58 -0500

    Did more of the lesson.

--- Topic 3 Lesson/Program.cs
+++ Topic 3 Lesson/Program.cs
@@ -4,8 +4,24 @@
     {
         static void Main(string[] args)
         {
-        String name, eyeColor, HairColor;
+            String name, eyes, teeth, hair;
             int age, height, weight;
+
+            name = "Linkin Krysa";
+            age = 16;
+            height = 181;    //inches
+            weight = 205;    //last time I checked
+            eyes = "Green";
+            hair = "Brown";
+
+            Console.WriteLine("Let's talk about " + name + ".");
+            Console.WriteLine("He's " + height + " inches tall");
+            Console.WriteLine("He's " + weight + " pounds heavy.");
+            Console.WriteLine("Actually, that's not too heavy.");
+            Console.WriteLine("His eyes are " + eyes + " and hair is " + hair);
+            Console.WriteLine();
+
+            Console.WriteLine("If I add " + age + ", " + height + ", and " + weight + " I get " + (age + height + weight) + ".");
         }
     }
 }
